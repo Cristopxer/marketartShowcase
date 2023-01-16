b3fad5f877fedb77b18d0774af464e466b84cf2b
cristopxer <cristopherortiz457@gmail.com>
2023-01-16 17:36:34 -0600

    upload to individual repo

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { Download, Features, SectionWrapper } from "./components";
+import assets from "./assets";
+import styles from "./styles/Global";
+
+const App = () => {
+  return (
+    <>
+      <SectionWrapper
+        title="Let's find a masterpiece"
+        description="Buy, store, collect art pieces from everyehere. Join 33+ million people using MarketArt"
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Smart user interface marketplace"
+        description="Experience and fluent interface with smooth and constant colors"
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWrapper
+        title="Deploymet"
+        description="MarketArt if built using Expo which runs natively on all users devices."
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="An user friendly interface"
+        description="The demo contains two different screens. The first screen shows the list of all art pieces availble and the second screen shows a detailed view of each art masterpiece."
+        mockupImg={assets.mockup}
+        banner="banner-02"
+      />
+      <Download />
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          Developed 💙 by Cristopher Ortiz
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Download.jsx
@@ -0,0 +1,35 @@
+import styles from "../styles/Global";
+import assets from "../assets";
+
+const Download = () => {
+  return (
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>
+            Check out the source code :)
+          </h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>
+            Get the full source code on Github
+          </p>
+        </div>
+        <a
+          href="https://github.com/Cristopxer/MarketArt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className={styles.btnPrimary}>Source Code</button>
+        </a>
+        <div className={styles.flexCenter}>
+          <img
+            src={assets.scene}
+            alt="download_png"
+            className={styles.fullImg}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Download;
